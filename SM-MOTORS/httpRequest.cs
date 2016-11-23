29d85be9f92ecdd955f43fb421797a859781549b
al-metal <al-metal@bk.ru>
2016-11-23 08:18:39 +0400

    Закрыл все соединения

--- SM-MOTORS/httpRequest.cs
+++ SM-MOTORS/httpRequest.cs
@@ -19,6 +19,7 @@ namespace Bike18
             res = (HttpWebResponse)req.GetResponse();
             StreamReader ressr = new StreamReader(res.GetResponseStream(), Encoding.GetEncoding(1251));
             string otv = ressr.ReadToEnd();
+            res.Close();
             return otv;
         }
 
@@ -33,6 +34,7 @@ namespace Bike18
             string otv = ressr.ReadToEnd();
             res.GetResponseStream().Close();
             req.GetResponse().Close();
+            res.Close();
 
             return otv;
         }
@@ -49,32 +51,25 @@ namespace Bike18
             Stream stre = req.GetRequestStream();
             stre.Close();
             HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            res.Close();
             return cooc;
         }
 
         public string PostRequest(CookieContainer cookie, string nethouseTovar)
         {
             string otv = null;
-            do
-            {
-                HttpWebResponse res = null;
-                HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(nethouseTovar);
-                req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
-                req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0";
-                req.Method = "POST";
-                req.ContentType = "application/x-www-form-urlencoded";
-                req.CookieContainer = cookie;
-                try
-                {
-                    res = (HttpWebResponse)req.GetResponse();
-                    StreamReader ressr = new StreamReader(res.GetResponseStream());
-                    otv = ressr.ReadToEnd();
-                }
-                catch (WebException e)
-                {
-                    break;
-                }
-            } while (otv == null);
+            HttpWebResponse res = null;
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(nethouseTovar);
+            req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0";
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.CookieContainer = cookie;
+            res = (HttpWebResponse)req.GetResponse();
+            StreamReader ressr = new StreamReader(res.GetResponseStream());
+            otv = ressr.ReadToEnd();
+            res.Close();
+
             return otv;
         }
     }
